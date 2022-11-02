a9208d363305932f9f7f1c3a753167470fcd2670
Alaa Obeid <alaaobed88@gmail.com>
2022-11-02 13:14:50 +0200

    returned after a long break, added api functions to fetch and display needed informations

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const apikey = '23c41db516346bf4179ca067f31a14699';
-
-const weatherUpdate = (data) => {
+let weather = { 
+    apikey : '3c41db516346bf4179ca067f31a14699',
     
+weatherUpdate : async function (city)  {
+    let fetching = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather.apikey}`)
+    let data = await fetching.json()
+     this.weatherDisplay(data); 
+
+},
+weatherDisplay : (data) => {
+ const { name } = data;
+ 
+ const { icon, description } = data.weather[0];
+ const { temp, humidity } = data.main;
+ const { speed } = data.wind;
+ console.log(name,description,icon,temp,humidity,speed);
+ },
 }
